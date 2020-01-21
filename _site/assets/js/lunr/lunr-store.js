35b1cb81bdc71692800ab715219a1dcda7490f31
jaags <jaags@live.com>
2020-01-21 11:57:16 -0800

    removed teaser images for all posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,32 +8,32 @@ var store = [{
         "excerpt":"Measuring Similarity in ML   ","categories": [],
         "tags": ["NLP","data science","similarities measure"],
         "url": "http://localhost:4000/measuring-similarity-in-ml/",
-        "teaser":"http://localhost:4000/images/IMG-20190710-WA0000.jpg"},{
+        "teaser":null},{
         "title": "Algebra1 Equations One Word",
         "excerpt":"example   ","categories": [],
         "tags": ["IOT","Internet of things"],
         "url": "http://localhost:4000/algebra1_equations_one_word/",
-        "teaser":"http://localhost:4000/images/node-red-banner-install.png"},{
+        "teaser":null},{
         "title": "Vector Norm",
         "excerpt":"The length of the vector is referred to as the vector norm or the vector’s magnitude. The length of a vector is a nonnegative number that describes the extent of the vector in space, and is sometimes referred to as the vector’s magnitude or the norm. — Page 112, No...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/vector_norm/",
-        "teaser":"http://localhost:4000/images/IMG-20190710-WA0000.jpg"},{
+        "teaser":null},{
         "title": "Broadcasting and the Softmax Function",
         "excerpt":"Softmax Function  ","categories": [],
         "tags": ["NLP","data science","similarities measure"],
         "url": "http://localhost:4000/broadcasting_softmax_function/",
-        "teaser":"http://localhost:4000/images/IMG-20190710-WA0000.jpg"},{
+        "teaser":null},{
         "title": "Interview Questions",
         "excerpt":"Differentiate between AI, ML and DP? Is Deep learning better than ML? Whatt is Perceptron? How does it work? What is the role of weights and bias? What are activation functions? What is the significance of a Cost/Loss function? What is gradient descent? What are the beenifits of mini-batch gradient...","categories": [],
         "tags": ["NLP","data science","similarities measure"],
         "url": "http://localhost:4000/interview_questions/",
-        "teaser":"http://localhost:4000/images/IMG-20190710-WA0000.jpg"},{
+        "teaser":null},{
         "title": "Math For Machine Learning",
         "excerpt":"Define Scalar?  Define Vector?  Define Matrice?   Scalars and Vectors  Vectos and Matrices   Real time examples with python code   References   Books  ","categories": [],
         "tags": ["data wrangling","data science","Extensions"],
         "url": "http://localhost:4000/math-for-machine-learning/",
-        "teaser":"http://localhost:4000/images/IMG-20190710-WA0000.jpg"},{
+        "teaser":null},{
         "title": "Neo4j Graph For Machine Learning",
         "excerpt":" ","categories": [],
         "tags": ["data wrangling","data science","Extensions"],
@@ -43,27 +43,27 @@ var store = [{
         "excerpt":"In a recent trends it’s more about applying machine learning in various uses cases. Here in this article, building a chrome extension would be one such.  ","categories": [],
         "tags": ["data wrangling","data science","Extensions"],
         "url": "http://localhost:4000/chrome-extension-example/",
-        "teaser":"http://localhost:4000/images/IMG-20190710-WA0000.jpg"},{
+        "teaser":null},{
         "title": "Machine Learning Production Deployment",
         "excerpt":" ","categories": [],
         "tags": ["data wrangling","data science","messy data"],
         "url": "http://localhost:4000/machine-learning-production-deployment/",
-        "teaser":"http://localhost:4000/images/perceptron/percept.jpg"},{
+        "teaser":null},{
         "title": "Datasets Links",
         "excerpt":"Public datasets in vision, nlp and more forked from caesar0301’s awesome datasets wiki. Agriculture U.S. Department of Agriculture’s PLANTS Database U.S. Department of Agriculture’s Nutrient Database Art Google’s Quick Draw Sketch Dataset Biology 1000 Genomes American Gut (Microbiome Project) Broad Bioimage Benchmark Collection (BBBC) Broad Cancer Cell Line Encyclopedia (CCLE)...","categories": [],
         "tags": ["data wrangling","dataset","data science","Extensions"],
         "url": "http://localhost:4000/datasets-links/",
-        "teaser":"http://localhost:4000/images/IMG-20190710-WA0000.jpg"},{
+        "teaser":null},{
         "title": "Data Science Resources",
         "excerpt":"Data Science Collected Resources A trove of carefully curated resource links (software, platforms, language, techniques, etc.) for data science, all in one place. Please feel free to add me here on LinkedIn if you are interested in data science and would like to connect. Artificial Intelligence MONTRÉAL.AI ACADEMY: ARTIFICIAL INTELLIGENCE...","categories": [],
         "tags": ["data wrangling","dataset","data science","Extensions"],
         "url": "http://localhost:4000/data-science-resources/",
-        "teaser":"http://localhost:4000/images/IMG-20190710-WA0000.jpg"},{
+        "teaser":null},{
         "title": "Node-RED Local Setup",
         "excerpt":"Node-RED Ready In the following, let’s install Node-RED on Ubuntu 16.04 LTS (Long Term Support) that requires Node.js and npm (node package manager). Installing Node.js Installing Node.js is easy on Ubuntu 16.04 LTS by typing in the below command on the terminal: $ sudo apt-get install nodejs Once the installation...","categories": [],
         "tags": ["IOT","Internet of things"],
         "url": "http://localhost:4000/node-red-local-setup/",
-        "teaser":"http://localhost:4000/images/node-red-banner-install.png"},{
+        "teaser":null},{
         "title": "Mobile Glass defects detection",
         "excerpt":"H1 Heading H2 Heading H3 Heading Here’s some basic text. And here’s some italics Here’s some bold text. What about a link? Here’s a bulleted list: First item Second item Third item Here’s a numbered list: First Second Third Python code block: import numpy as np def test_function(x, y): z...","categories": [],
         "tags": ["data wrangling","data science","messy data"],
@@ -78,7 +78,7 @@ var store = [{
         "excerpt":"H1 Heading H2 Heading H3 Heading Here’s some basic text. And here’s some italics Here’s some bold text. What about a link? Here’s a bulleted list: First item Second item Third item Here’s a numbered list: First Second Third Python code block: import numpy as np def test_function(x, y): z...","categories": [],
         "tags": ["data wrangling","dataset","data science","messy data"],
         "url": "http://localhost:4000/visualization-for-data/",
-        "teaser":"http://localhost:4000/images/IMG_20181114_165012.jpg"},{
+        "teaser":null},{
         "title": "Relational Algebra & SQL",
         "excerpt":"Query languages (computer languages used to make queries in databases) are abundant in style and usage today. In 2018, Microsoft released Azure Data Explorer to GA, introducing another query language that is specifically optimized for semi-structured, unstructured, and time-series data. Even though we have many querying language options, they are...","categories": [],
         "tags": ["data","math","SQL"],
